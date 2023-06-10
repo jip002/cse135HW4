98a46f9e5881ae2dff19cca40b689d0b318f4ce3
Kevin Zhang <jiz100@ucsd.edu>
2023-06-09 23:05:16 -0700

    update db route

---
+++ routes/db.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+const { User } = require('../models/user');
+
+router.use(express.json());
+router.use(bodyParser.json());
+
+router.get('/info', async(req, res)=>{
+    console.log("works");
+    try {
+        const users = await User.find({});
+        res.status(200).json(users);
+    }
+    catch (error){
+        res.status(500).json({message: error.message}); 
+    }
+})
+router.post('/info', async(req, res)=>{
+    console.log('--POST 200 ---');
+    const user = new User({
+        email: req.body.email,
+        password: req.body.password, //hash the password. 
+        admin: false
+    }) 
+    //type email and pw is enough; no nee to fill admin and id. 
+    const resultUser = await user.save();
+    console.log(resultUser);
+    res.status(200).send(await User.find());
+    // res.status(200).json(req.body);
+})
+router.delete('/info', async(req, res)=>{
+    console.log('here');
+    try {
+        
+        const removed = await User.findByIdAndRemove(req.params.id);
+        res.status(200).json(removed);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
+router.put('/info', async(req, res)=>{
+    console.log('put');
+    try {
+        const updated = await User.findByIdAndUpdate(req.params.id,req.body, { new: true });
+        res.status(200).json(updated);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
+router.patch('/info', async(req, res)=>{
+    console.log('patch');
+    try {
+        const updated = await User.findByIdAndUpdate(req.params.id,{$set: req.body}, { new: true });
+        res.status(200).json(updated);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
+module.exports = router;
