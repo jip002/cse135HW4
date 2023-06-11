70f2f2e59ee02fc9c74e636465cd12ce7acdad55
jip002 <jip002@uscd.edu>
2023-06-10 19:14:33 -0700

    logout fix

--- routes/logout.js
+++ routes/logout.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const {checkAuthenticated} = require('../middleware/checkAuth');
 
-router.delete('/', (req, res) => {
+router.delete('/', checkAuthenticated, (req, res) => {
     req.logout(function(err) {
         if (err) { return next(err); }
         res.render('logout.ejs');
